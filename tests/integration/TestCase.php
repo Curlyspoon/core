faeea75ff2d7dbef05db95a75b213a2596b05c34
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-08 07:39:08 +0000

    Apply fixes from StyleCI

--- tests/integration/TestCase.php
+++ tests/integration/TestCase.php
@@ -1,9 +1,9 @@
 <?php
 
-use PHPUnit\Framework\TestCase as PhpUnitTestCase;
 use Curlyspoon\Core\Managers\ElementManager;
 use Curlyspoon\Core\Tests\Elements\Headline;
 use Curlyspoon\Core\Tests\Elements\UpperHeadline;
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
 
 abstract class TestCase extends PhpUnitTestCase
 {
