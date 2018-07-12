9a60ad3505ac9787170d609e32f37bf664731d9e
Tom Witkowski <dev.gummibeer@gmail.com>
2018-07-12 16:22:59 +0200

    Revert "prevent conflicts with locked resolvers"
    This reverts commit 572ab54

--- src/Elements/Element.php
+++ src/Elements/Element.php
@@ -46,6 +46,11 @@ abstract class Element implements ElementContract
      */
     protected $options = [];
 
+    /**
+     * @var OptionsResolver
+     */
+    protected $resolver;
+
     public function __construct(array $options = [])
     {
         $this->setOptions($options);
@@ -100,23 +105,25 @@ abstract class Element implements ElementContract
 
     protected function optionsResolver(): OptionsResolver
     {
-        $resolver = new OptionsResolver();
-        $resolver->setDefaults($this->defaults);
+        if (is_null($this->resolver)) {
+            $this->resolver = new OptionsResolver();
+            $this->resolver->setDefaults($this->defaults);
 
-        $resolver->setRequired($this->required);
+            $this->resolver->setRequired($this->required);
 
-        foreach ($this->types as $option => $types) {
-            $resolver->setAllowedTypes($option, $types);
+            foreach ($this->types as $option => $types) {
+                $this->resolver->setAllowedTypes($option, $types);
+            }
+
+            foreach ($this->values as $option => $values) {
+                $this->resolver->setAllowedValues($option, $values);
+            }
+
+            if (method_exists($this, 'configureOptions')) {
+                $this->configureOptions($this->resolver);
+            }
         }
 
-        foreach ($this->values as $option => $values) {
-            $resolver->setAllowedValues($option, $values);
-        }
-
-        if (method_exists($this, 'configureOptions')) {
-            $this->configureOptions($resolver);
-        }
-
-        return $resolver;
+        return $this->resolver;
     }
 }
