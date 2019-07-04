85a5805386e8a5f6146f1578620a0033ae5a429f
Gummibeer <dev.gummibeer@gmail.com>
2019-07-04 12:45:26 +0200

    add possible type constants

--- src/Elements/Element.php
+++ src/Elements/Element.php
@@ -7,6 +7,38 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class Element implements ElementContract
 {
+    const TYPE_SCALAR = 'scalar';
+    const TYPE_STRING = 'string';
+    const TYPE_BOOL = 'bool';
+
+    const TYPE_INT = 'int';
+    const TYPE_LONG = 'long';
+
+    const TYPE_FLOAT = 'float';
+    const TYPE_DOUBLE = 'double';
+    const TYPE_REAL = 'real';
+
+    const TYPE_NUMERIC = 'numeric';
+    const TYPE_NAN = 'nan';
+    const TYPE_FINITE = 'finite';
+    const TYPE_INFINITE = 'infinite';
+
+    const TYPE_NULL = 'null';
+    const TYPE_ARRAY = 'array';
+    const TYPE_OBJECT = 'object';
+    const TYPE_CALLABLE = 'callable';
+    const TYPE_COUNTABLE = 'countable';
+    const TYPE_ITERABLE = 'iterable';
+    const TYPE_RESOURCE = 'resource';
+
+    const TYPE_DIR = 'dir';
+    const TYPE_FILE = 'file';
+    const TYPE_LINK = 'link';
+    const TYPE_READABLE = 'readable';
+    const TYPE_WRITABLE = 'writable';
+    const TYPE_WRITEABLE = 'writeable';
+    const TYPE_EXECUTABLE = 'executable';
+
     /**
      * @var string
      */
